f8334a96c894393a708f20dcfe61a1f66be630e9
Jaime M <j7isaac@gmail.com>
2016-02-07 05:53:02 +0000

    Set up default URL options (production)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Chessapp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = { host: 'https://thecheckmates-chessapp.herokuapp.com/' }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
