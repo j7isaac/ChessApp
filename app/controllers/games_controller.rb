88a06cd339cb8f1b554610afb4803075a17dc6ff
j7isaac <j7issac@gmail.com>
2016-02-18 16:40:22 -0600

    Def New, Create and Show

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,23 @@
 class GamesController < ApplicationController
 	before_action :authenticate_player!
+	helper_method :game
 
   def new
+  	@game = Game.new
   end
+
+  def create
+    @game = Game.create(game_params)
+    redirect_to game_path(@game)
+  end
+
+  def show
+  end
+
+  private
+
+  def game
+    @game ||= Game.where(id: params[:id]).last
+  end
+
 end
