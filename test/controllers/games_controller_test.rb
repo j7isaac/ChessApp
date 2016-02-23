4db7eed6e912ed154cc4051e546d91f1b812ec61
j7isaac <j7issac@gmail.com>
2016-02-23 15:30:24 -0600

    Added test for Game controller: new and create

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class GamesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "should get new with Create game" do
+		player1 = players(:player1)
+		sign_in player1
+		get :new, {}, { white_player_id: player1.id }
+		game = assigns(:game)
+		assert game.new_record?
+		assert_response :success
+	end
+
+	test "should create game" do
+		player = players(:player2)
+		sign_in player
+		params = {
+			game: {
+				white_player_id: player.id,
+      	black_player_id: nil
+			}
+		}
+		post :create, params, { white_player_id: player.id }
+		game = assigns(:game)
+		assert game.persisted?
+		assert_redirected_to game_url(game)
+	end
+
+
+
 end
