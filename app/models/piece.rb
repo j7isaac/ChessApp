7e831cbbfec5b05fd950b3f4b6f4c379552d103d
Sandra Harrasser <design@sandraharrasser.com>
2016-02-26 20:15:20 +0000

    adjusted the method name to reflect is_obstructed

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,7 +1,7 @@
 class Piece < ActiveRecord::Base
   belongs_to :game
 
-  def obstructed_move?(x, y) # WORK IN PROGRESS
+  def is_obstructed?(x, y) # WORK IN PROGRESS
     obstructed_space = a_piece_at_its_space(x, y)
     return false if obstructed_space.empty?
     return true if obstructed_space #is not empty
