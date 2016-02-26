325db2de58b37eac174d3880240f00b321efe393
Sandra Harrasser <design@sandraharrasser.com>
2016-02-26 20:01:36 +0000

    added a work in progress version of obstructed method

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,3 +1,12 @@
 class Piece < ActiveRecord::Base
   belongs_to :game
+
+  def obstructed_move?(x, y) # WORK IN PROGRESS
+    obstructed_space = a_piece_at_its_space(x, y)
+    return false if obstructed_space.empty?
+    return true if obstructed_space #is not empty
+    end
+    end
+  end
+
 end
