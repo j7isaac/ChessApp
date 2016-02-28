3ab87e86d58539803da9ce5819ad01931d8ea956
Sandra Harrasser <design@sandraharrasser.com>
2016-02-28 00:39:51 +0000

    adjusted code to locate pieces at x/y position

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,11 +2,7 @@ class Piece < ActiveRecord::Base
   belongs_to :game
 
   def is_obstructed?(x, y) # WORK IN PROGRESS
-    obstructed_space = a_piece_at_its_space(x, y)
-    return false if obstructed_space.empty?
-    return true if obstructed_space #is not empty
-    end
-    end
+    pieces.where(x_coordinate: x, y_coordinate: y)
   end
 
 end
