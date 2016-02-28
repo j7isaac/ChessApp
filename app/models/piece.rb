4d1554d65a55c4d825eb02532315fa6aa2a387cb
Sandra Harrasser <design@sandraharrasser.com>
2016-02-28 01:57:42 +0000

    I think this logic works

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,7 +2,10 @@ class Piece < ActiveRecord::Base
   belongs_to :game
 
   def is_obstructed?(x, y) # WORK IN PROGRESS
-    pieces.where(x_coordinate: x, y_coordinate: y)
+  	#query if piece is located at the x, y array space
+    pieces.where(x_coordinate: x, y_coordinate: y).last
+    #with the valid move path from the last position
+    #of the piece. 
   end
 
 end
