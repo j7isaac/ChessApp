b5c21614014e5e35a73b7efb218c117311e28113
nsiderwolverine <justin.joseph0@yahoo.com>
2016-03-01 22:15:34 +0000

    Add capture support

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,6 +1,5 @@
 class PiecesController < ApplicationController
   before_action :set_piece, only: [:show, :update]
-  before_action :set_friendly_pieces, only: :update
   
   def show
     @game = Piece.find(params[:id]).game
@@ -11,7 +10,7 @@ class PiecesController < ApplicationController
     x = params[:piece][:x_coordinate].to_i
     y = params[:piece][:y_coordinate].to_i
 
-    if @piece.valid_move?(x, y) && !friendly_piece_occupies_destination?(x, y)
+    if @piece.move_to!(x, y)
       flash[:success] = "Move was valid" if @piece.update_attributes(piece_params)
     else
       flash[:danger] = "Move was invalid"
@@ -23,21 +22,7 @@ class PiecesController < ApplicationController
   private
   
     def set_piece
-      @piece = Piece.find(params[:id])
-    end
-    
-    def set_friendly_pieces
-    # Collect non-captured friendly pieces, but not the currently-moving piece
-      @friendly_pieces ||= @piece.game.pieces.where(color: @piece.color, captured?: false).where.not(id: @piece)      
-    end
-    
-    def friendly_piece_occupies_destination?(x, y)
-    # Check if a friendly piece occupies the targeted destination  
-      @friendly_pieces.each do |friendly_piece|
-        return true if friendly_piece.x_coordinate == x && friendly_piece.y_coordinate == y
-      end
-      
-      false
+      @piece ||= Piece.find(params[:id])
     end
   
     def piece_params
--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,3 +1,58 @@
 class Piece < ActiveRecord::Base
   belongs_to :game
+  
+# Allows piece to move if the move is valid and a friendly piece does not occupy the target destination
+# Also allows pieces to be captured
+  def move_to!(x, y)
+  # Set friendly pieces
+    set_friendly_pieces
+  # Set enemy pieces
+    set_enemy_pieces
+
+  # Immediately return to controller with false if move is determined invalid
+    return false if !valid_move? x, y
+
+  # Return to controller with false if a friendly piece occupies the target destination
+    return false if friendly_piece_occupies_destination? x, y
+
+  # Return to controller with true if an enemy piece is captured
+    return true if capture_piece? x, y
+
+  # The move is still valid if the processing reaches this point
+    true
+  end
+  
+  private
+
+    def set_friendly_pieces
+    # Collect remaining friendly pieces, but not the currently-moving piece
+      @friendly_pieces ||= game.pieces.where(color: color).where.not(id: self)
+    end
+
+    def set_enemy_pieces
+    # Collect remaining enemy pieces
+      @enemy_pieces ||= game.pieces.where.not(color: color)
+    end
+
+    def friendly_piece_occupies_destination?(x, y)
+    # Check if a friendly piece occupies the targeted destination  
+      @friendly_pieces.each do |friendly_piece|
+        return true if friendly_piece.x_coordinate == x && friendly_piece.y_coordinate == y
+      end
+      
+      false
+    end
+    
+    def capture_piece?(x, y)
+    # Check if an enemy piece occupies the targeted destination  
+      @enemy_pieces.each do |enemy_piece|
+        if enemy_piece.x_coordinate == x && enemy_piece.y_coordinate == y
+        # Remove the piece from the database if being captured
+          enemy_piece.destroy
+          return true
+        end
+      end
+      
+      false
+    end
 end
