5ce3f56341ec7f5991da34ae378750ae8791883c
nsiderwolverine <justin.joseph0@yahoo.com>
2016-03-04 00:37:54 +0000

    Finish capture logic; clean up, update, correct, and add to Pieces test

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,143 +1,132 @@
 class Piece < ActiveRecord::Base
   belongs_to :game
 
-# Allows piece to move if the move is valid and a friendly piece does not occupy the target destination
-# Also allows pieces to be captured
   def move_to!(x, y)
-  # Set friendly pieces
-    # set_friendly_pieces
-  # Set enemy pieces
-    # set_enemy_pieces
-
-  # Immediately return to controller with false if move is determined invalid
+  # Immediately return to controller with false if move is deemed invalid
     return false if !valid_move? x, y
+
+  # Return to controller with false if moving piece is obstructed
+    return false if is_obstructed? x, y
 
   # Return to controller with false if a friendly piece occupies the target destination
     return false if friendly_piece_occupies_destination? x, y
 
   # Return to controller with true if an enemy piece is captured
-    return true if capture_piece? x, y
+    return true if enemy_piece_captured? x, y
 
-  # The move is still valid if the processing reaches this point
+  # The move remains valid if the processing reaches this point
     true
   end
-  
-  private
 
-    def set_friendly_pieces
-    # Collect remaining friendly pieces, but not the currently-moving piece
-      # @friendly_pieces ||= game.pieces.where(color: color).where.not(id: self)
-    end
-
-    def set_enemy_pieces
-    # Collect remaining enemy pieces
-      # @enemy_pieces ||= game.pieces.where.not(color: color)
-    end
-
-    def friendly_piece_occupies_destination?(x, y)
-    # Check if a friendly piece occupies the targeted destination
-      #game.pieces.where(x_coordinate: x, y_coordinate: y).any? ? true : false
-      @friendly_pieces.each do |friendly_piece|
-        return true if friendly_piece.x_coordinate == x && friendly_piece.y_coordinate == y
-      end
-      
-      false
-    end
-    
-    def capture_piece?(x, y)
-    # Check if an enemy piece occupies the targeted destination  
-      # enemy_piece = game.pieces.where.not(color: color).where(x_coordinate: x, y_coordinate: y)
-      # return true if enemy_piece && enemy_piece.destroy
-      # false
-      @enemy_pieces.each do |enemy_piece|
-        if enemy_piece.x_coordinate == x && enemy_piece.y_coordinate == y
-        # Remove the piece from the database if being captured
-          enemy_piece.destroy
-          return true
-        end
-      end
-      
-      false
-    end
-
-    # Returns true/false if a piece is obstructed in its movement.
-    # Errors for movements not diagonal, vertical or horizontal
+  # Returns true/false if a piece is obstructed in its movement.
+  # Errors for movements not diagonal, vertical or horizontal
     def is_obstructed?(x, y)
-      # Checks if method is dealing with a diagonal, vertical or horzonital movement.
-      # If not error is raised.
-      if x == x_coordinate or y == y_coordinate
-        # Check each item in given array for an obstruction using contains_piece? method.
+    # Immediately return false if moving piece is a Knight, which is exempt from obstruction
+      return false if type.eql? 'Knight'
+
+    # Checks if method is dealing with a diagonal, vertical or horizontal movement.
+    # If not error is raised.
+      if x == x_coordinate || y == y_coordinate
+      # Check each item in given array for an obstruction using contains_piece? method.
         horizontal_vertical_array(x, y).any? {|h, v| game.contains_piece?(h, v) }
       elsif (x_coordinate - x).abs == (y_coordinate - y).abs
-        # Check each item in given array for an obstruction using contains_piece? method.
+      # Check each item in given array for an obstruction using contains_piece? method.
         diagonal_array(x, y).any? {|h, v| game.contains_piece?(h, v) }
       else
-        raise ArgumentError, 'Invalid input. Not diagonal, horizontal, or vertical.'
+        raise ArgumentError, 'Invalid move: not diagonal, horizontal, or vertical.'
       end
     end
   
-    # Outputs an array of coordinates between Piece and destination in a horizontal/vertical movement.
-    # Does not include destination coordinate nor Piece coordinate.
+  # Outputs an array of coordinates between Piece and destination in a horizontal/vertical movement.
+  # Does not include destination coordinate or Piece coordinate.
     def horizontal_vertical_array(x, y)
-      # Create array
+    # Create array
       pathway_spaces = []
-      # Make coordinates available locally to avoid erroneous changes.
+
+    # Make coordinates available locally to avoid erroneous changes.
       pos_x = x_coordinate
       pos_y = y_coordinate
-      # Checks what axis the movement is relative to.
+
+    # Checks what axis the movement is relative to.
       if y == pos_y
-        # Store increment values to be used for directional purposes.
+      # Store increment values to be used for directional purposes.
         x_increment = x > pos_x ? 1 : -1
-        # Ensure Piece coordinate is excluded from array.
+      # Ensure Piece coordinate is excluded from array.
         pos_x += x_increment
-        # Loop through values stopping before 0.
+
+      # Loop through values stopping before 0.
         while (x - pos_x).abs > 0
-          # Push the value to array.
+        # Push the value to array.
           pathway_spaces << [pos_x, pos_y]
-          # Move to next space using increment variable.
+        # Move to next space using increment variable.
           pos_x += x_increment
         end
       elsif x == pos_x
-        # Store increment values to be used for directional purposes.
+      # Store increment values to be used for directional purposes.
         y_increment = y > pos_y ? 1 : -1
-        # Ensure Piece coordinate is excluded from array.
+      # Ensure Piece coordinate is excluded from array.
         pos_y += y_increment
-        # Loop through values stopping before 0.
+        
+      # Loop through values stopping before 0.
         while (y - pos_y).abs > 0
-          # Push the value to array.
+        # Push the value to array.
           pathway_spaces << [pos_x, pos_y]
-          # Change value to next space using increment variable.
+        # Change value to next space using increment variable.
           pos_y += y_increment
         end
       end
+
+    # Return array
+      pathway_spaces
+    end
+  
+  # Outputs an array of coordinates between Piece and destination in a diagonal movement.
+  # Does not include destination coordinate or Piece coordinate.
+    def diagonal_array(x, y)
+    # Create array
+      pathway_spaces = []
+
+    # Make coordinates available locally to avoid erroneous changes.
+      pos_x = x_coordinate
+      pos_y = y_coordinate
+
+    # Store increment values to be used for directional purposes.
+      x_increment = x > pos_x ? 1 : -1
+      y_increment = y > pos_y ? 1 : -1
+
+    # Ensure Piece coordinate is excluded from array.
+      pos_x += x_increment
+      pos_y += y_increment
+
+    # Loop through x and y values stopping before [0, 0].
+      while (x - pos_x).abs > 0 && (y - pos_y).abs > 0
+      # Push the value to array.
+        pathway_spaces << [pos_x, pos_y]
+      # Change value to next space using increment variable.
+        pos_x += x_increment
+        pos_y += y_increment
+      end
+
       # Return array
       pathway_spaces
     end
   
-    # Outputs an array of coordinates between Piece and destination in a diagonal movement.
-    # Does not include destination coordinate nor Piece coordinate.
-    def diagonal_array(x, y)
-      # Create array
-      pathway_spaces = []
-      # Make coordinates available locally to avoid erroneous changes.
-      pos_x = x_coordinate
-      pos_y = y_coordinate
-      # Store increment values to be used for directional purposes.
-      x_increment = x > pos_x ? 1 : -1
-      y_increment = y > pos_y ? 1 : -1
-      # Ensure Piece coordinate is excluded from array.
-      pos_x += x_increment
-      pos_y += y_increment
-      # Loop through x and y values stopping before [0, 0].
-      while (x - pos_x).abs > 0 && (y - pos_y).abs > 0
-        # Push the value to array.
-        pathway_spaces << [pos_x, pos_y]
-        # Change value to next space using increment variable.
-        pos_x += x_increment
-        pos_y += y_increment
-      end
-      # Return array
-      pathway_spaces
+  private
+
+    def friendly_piece_occupies_destination?(x, y)
+    # Query for friendly piece in target destination
+      friendly_piece = game.pieces.where(x_coordinate: x, y_coordinate: y, color: color).last
+    
+    # Check if a friendly piece occupies the targeted destination  
+      friendly_piece ? true : false
+    end
+
+    def enemy_piece_captured?(x, y)
+    # Query for enemy piece in target destination
+      enemy_piece = game.pieces.where.not(color: color).where(x_coordinate: x, y_coordinate: y).last
+
+    # Check if an enemy piece occupies the target destination and is successfully captured
+      ( enemy_piece && enemy_piece.destroy ) ? true : false
     end
 
 end
--- test/models/piece_test.rb
+++ test/models/piece_test.rb
@@ -1,42 +1,61 @@
 require 'test_helper'
 
 class PieceTest < ActiveSupport::TestCase
+
   def setup
     @game = games(:one)
+
     @A6 = Bishop.create(game: @game, color: 'white', x_coordinate: 1, y_coordinate: 6)
     @C4 = Pawn.create(game: @game, color: 'black', x_coordinate: 3, y_coordinate: 4)
     @F1 = Bishop.create(game: @game, color: 'black', x_coordinate: 6, y_coordinate: 1)
     @E2 = Pawn.create(game: @game, color: 'black', x_coordinate: 5, y_coordinate: 2)
     @A1 = Rook.create(game: @game, color: 'black', x_coordinate: 1, y_coordinate: 1)
+    @E5 = Pawn.create(game: @game, color: 'black', x_coordinate: 5, y_coordinate: 5)
+    @A8 = Rook.create(game: @game, color: 'white', x_coordinate: 1, y_coordinate: 8)
     @A8 = Rook.create(game: @game, color: 'white', x_coordinate: 1, y_coordinate: 8)
     @A2 = Pawn.create(game: @game, color: 'black', x_coordinate: 1, y_coordinate: 2)
     @D4 = Knight.create(game: @game, color: 'white', x_coordinate: 4, y_coordinate: 4)
+    @E7 = Queen.create(game: @game, color: 'white', x_coordinate: 5, y_coordinate: 7)
+    @F6 = Rook.create(game: @game, color: 'white', x_coordinate: 6, y_coordinate: 6)
   end
 
-  test 'is_obstructed? method - Should correctly determine if a piece is obstructed with true or false' do
-    # is_obstructed? A6 -> C4 => false
+  test "is_obstructed? - should correctly determine if a piece is obstructed" do
+  # A6 -> C4 => false
     assert_not @A6.is_obstructed?(3, 4), "Should be false"
-    # is_obstructed? F1 -> D3 => true
+  # F1 -> D3 => true
     assert @F1.is_obstructed?(4, 3), "Should be true"
-    # is_obstructed? A1 -> A4 => true
+  # A1 -> A4 => true
     assert @A1.is_obstructed?(1, 4), "Should be true"
-    # is_obstructed? D4 -> B5 => Raise an Error Message # invalid input.  Not diagnal, horizontal, or vertical.
-    assert_raises(ArgumentError) { @D4.is_obstructed?(2, 5) }
-    #is_obstructed? A8 -> A6 => false
+  # E7 -> C6 => Raise error # Invalid move: not diagnal, horizontal, or vertical.
+    assert_raises(ArgumentError) { @E7.is_obstructed?(3, 6) }
+  # D4 -> B5 => false
+    assert_not @D4.is_obstructed?(2, 5), "Should be false"
+  # A8 -> A6 => false
     assert_not @A8.is_obstructed?(1, 6), "Should be false"
-    # is_obstructed? A8 -> C8 => false
+  # A8 -> C8 => false
     assert_not @A8.is_obstructed?(3, 8), "Should be false"
   end
 
-  test 'should find positions between two points in vertical movement' do
-    assert_equal([[1, 2], [1, 3]], @A1.horizontal_vertical_array(1, 4))
+  test "should find positions between two points in vertical movement" do
+    assert_equal [[1, 2], [1, 3]], @A1.horizontal_vertical_array(1, 4)
   end
 
-  test 'should find positions between two points in horizontal movement' do
-    assert_equal([[2, 8]], @A8.horizontal_vertical_array(3, 8))
+  test "should find positions between two points in horizontal movement" do
+    assert_equal [[2, 8]], @A8.horizontal_vertical_array(3, 8)
   end
 
-  test 'should find positions between two points in diagonal movement' do
-    assert_equal([[5, 2]], @F1.diagonal_array(4, 3))
+  test "should find positions between two points in diagonal movement" do
+    assert_equal [[5, 2]], @F1.diagonal_array(4, 3)
   end
+  
+  test "capturing of pieces" do
+  # Test white Knight capturing a black Pawn
+    assert @D4.move_to!(5, 2), "White knight can't capture black pawn?"
+    assert_not Piece.exists?(@E2), "Black pawn not captured?"
+
+  # Test black pawn capturing a white Rook
+    assert @E5.move_to!(6, 6), "Black Pawn can't capture white rook?"
+    assert_not Piece.exists?(@F6), "White rook not captured?"
+  end
+
 end
