cde00cfd414da2c174177988a0e28e5a89164486
nsiderwolverine <justin.joseph0@yahoo.com>
2016-03-13 02:20:15 +0000

    Add tests for causing and preventing check logic

--- test/models/piece_test.rb
+++ test/models/piece_test.rb
@@ -5,18 +5,27 @@ class PieceTest < ActiveSupport::TestCase
   def setup
     @game = games(:one)
 
-    @A6 = Bishop.create(game: @game, color: 'white', x_coordinate: 1, y_coordinate: 6)
-    @C4 = Pawn.create(game: @game, color: 'black', x_coordinate: 3, y_coordinate: 4)
-    @F1 = Bishop.create(game: @game, color: 'black', x_coordinate: 6, y_coordinate: 1)
-    @E2 = Pawn.create(game: @game, color: 'black', x_coordinate: 5, y_coordinate: 2)
     @A1 = Rook.create(game: @game, color: 'black', x_coordinate: 1, y_coordinate: 1)
-    @E5 = Pawn.create(game: @game, color: 'black', x_coordinate: 5, y_coordinate: 5)
-    @A8 = Rook.create(game: @game, color: 'white', x_coordinate: 1, y_coordinate: 8)
-    @A8 = Rook.create(game: @game, color: 'white', x_coordinate: 1, y_coordinate: 8)
     @A2 = Pawn.create(game: @game, color: 'black', x_coordinate: 1, y_coordinate: 2)
+    @A6 = Bishop.create(game: @game, color: 'white', x_coordinate: 1, y_coordinate: 6)
+    @A8 = Rook.create(game: @game, color: 'white', x_coordinate: 1, y_coordinate: 8)
+    
+    @C4 = Pawn.create(game: @game, color: 'black', x_coordinate: 3, y_coordinate: 4)
+    @C6 = Pawn.create(game: @game, color: 'black', x_coordinate: 3, y_coordinate: 6)
+    @C5 = King.create(game: @game, color: 'black', x_coordinate: 3, y_coordinate: 6)
+    @C7 = Knight.create(game: @game, color: 'black', x_coordinate: 3, y_coordinate: 7)
+    
     @D4 = Knight.create(game: @game, color: 'white', x_coordinate: 4, y_coordinate: 4)
+
+    @E2 = Pawn.create(game: @game, color: 'black', x_coordinate: 5, y_coordinate: 2)
+    @E5 = Pawn.create(game: @game, color: 'black', x_coordinate: 5, y_coordinate: 5)
+    @E6 = Rook.create(game: @game, color: 'black', x_coordinate: 5, y_coordinate: 6)
     @E7 = Queen.create(game: @game, color: 'white', x_coordinate: 5, y_coordinate: 7)
+    
+    @F1 = Bishop.create(game: @game, color: 'black', x_coordinate: 6, y_coordinate: 1)
     @F6 = Rook.create(game: @game, color: 'white', x_coordinate: 6, y_coordinate: 6)
+    
+    @H8 = King.create(game: @game, color: 'white', x_coordinate: 8, y_coordinate: 8)
   end
 
   test "is_obstructed? - should correctly determine if a piece is obstructed" do
@@ -37,15 +46,15 @@ class PieceTest < ActiveSupport::TestCase
   end
 
   test 'should find positions between two points in vertical movement' do
-    assert_equal([[1, 2], [1, 3]], @A1.pathway_array(1, 4))
+    assert_equal [[1, 2], [1, 3]], @A1.pathway_array(1, 4)
   end
 
   test 'should find positions between two points in horizontal movement' do
-    assert_equal([[2, 8]], @A8.pathway_array(3, 8))
+    assert_equal [[2, 8]], @A8.pathway_array(3, 8)
   end
 
   test 'should find positions between two points in diagonal movement' do
-    assert_equal([[5, 2]], @F1.pathway_array(4, 3))
+    assert_equal [[5, 2]], @F1.pathway_array(4, 3)
   end
   
   test "capturing of pieces" do
@@ -56,6 +65,28 @@ class PieceTest < ActiveSupport::TestCase
   # Test black pawn capturing a white Rook
     assert @E5.move_to!(6, 6), "Black Pawn can't capture white rook?"
     assert_not Piece.exists?(@F6), "White rook not captured?"
+  end
+
+  test "should cause check" do
+  # Test white Rook putting black King in check
+    @F6.move_to! 6, 5
+    assert @F6.game.in_check?(@F6.color), "Game not in check?"
+  end
+
+  test "should prevent entering check" do
+  # Test black King avoiding moving into check
+    assert_not @C5.move_to! 3, 6
+    assert_not @C5.game.in_check?(@C5.color), "Black King entered check?"
+  end
+
+  test "should eliminate check" do
+  # Test that a black Rook rescues black King from being in check
+    @C5.move_to! 4, 5
+    @F6.move_to! 6, 5
+    assert @F6.game.in_check?(@F6.color), "Game not in check?"
+
+    @E6.move_to! 5, 5
+    assert_not @E6.game.in_check?(@E6.color), "Game still in check?"
   end
 
 end
