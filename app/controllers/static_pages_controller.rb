70e7b835b300de62e0fbffebc1bd3d06d5776296
j7isaac <j7issac@gmail.com>
2016-03-21 16:07:09 -0500

    Styled the Open Game List

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
 	
 	def index
-		@open_games = Game.where(black_player_id: nil).where.not(white_player_id: current_player.id).first(20) if player_signed_in?
+		@open_games = Game.where(black_player_id: nil).where.not(white_player_id: current_player.id).first(12) if player_signed_in?
 	end
 	
 end
