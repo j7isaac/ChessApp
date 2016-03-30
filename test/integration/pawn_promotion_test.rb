9365669a2ab6ea06843a9871ceae4fcb32a309e4
justinrjoseph <justin.joseph0@yahoo.com>
2016-03-30 21:01:48 +0000

    Add king pieces to pawn promotion integration test setup to satisfy check logic; tweak black king and pawn coordinates in piece test to fix prevent check test

--- test/integration/pawn_promotion_test.rb
+++ test/integration/pawn_promotion_test.rb
@@ -4,46 +4,49 @@ class PawnPromotionTest < ActionDispatch::IntegrationTest
   
   def setup
     @game = games(:one)
+
+    @H5 = King.create(game: @game, color: 'white', x_coordinate: 8, y_coordinate: 5)
+    @A5 = King.create(game: @game, color: 'black', x_coordinate: 1, y_coordinate: 5)
     
     @B1 = Pawn.create(game: @game, color: 'white', x_coordinate: 2, y_coordinate: 1)
     @G8 = Pawn.create(game: @game, color: 'black', x_coordinate: 7, y_coordinate: 8)
   end
 
   test "should execute valid Queen promotion for white pawn" do
-    validate_promotion @B1, "Queen", 1, 1, "white"
+    validate_promotion @B1, 1, 1, "Queen", "white"
   end
 
   test "should execute valid Knight promotion for white pawn" do
-    validate_promotion @B1, "Knight", 1, 1, "white"
+    validate_promotion @B1, 1, 1, "Knight", "white"
   end
 
   test "should execute valid Rook promotion for white pawn" do
-    validate_promotion @B1, "Rook", 1, 1, "white"
+    validate_promotion @B1, 1, 1, "Rook", "white"
   end
 
   test "should execute valid Bishop promotion for white pawn" do
-    validate_promotion @B1, "Bishop", 1, 1, "white"
+    validate_promotion @B1, 1, 1, "Bishop", "white"
   end
 
   test "should execute valid Queen promotion for black pawn" do
-    validate_promotion @G8, "Queen", 8, 8, "black"
+    validate_promotion @G8, 8, 8, "Queen", "black"
   end
   
   test "should execute valid Knight promotion for black pawn" do
-    validate_promotion @G8, "Knight", 8, 8, "black"
+    validate_promotion @G8, 8, 8, "Knight", "black"
   end
   
   test "should execute valid Rook promotion for black pawn" do
-    validate_promotion @G8, "Rook", 8, 8, "black"
+    validate_promotion @G8, 8, 8, "Rook", "black"
   end
   
   test "should execute valid Bishop promotion for black pawn" do
-    validate_promotion @G8, "Bishop", 8, 8, "black"
+    validate_promotion @G8, 8, 8, "Bishop", "black"
   end
 
   private
   
-    def validate_promotion(piece, promotion, x, y, color)
+    def validate_promotion(piece, x, y, promotion, color)
       put game_piece_path(game_id: @game, id: piece, piece: { id: piece, x_coordinate: x, y_coordinate: y, type: promotion })
       promoted_piece = Game.find(@game.id).pieces.where(color: color).last
       assert_instance_of promotion.constantize, promoted_piece, "Pawn not promoted to #{promotion}?"
