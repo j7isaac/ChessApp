19147043742a45e6858cf7dd1fdf1bd2bc9c82e3
justinrjoseph <justin.joseph0@yahoo.com>
2016-04-08 00:04:16 +0000

    Refactor player leave code (games controller) and add white player leave integration tests

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -15,32 +15,12 @@ class GamesController < ApplicationController
 
   def update
     if white_leaves_as_only_player?
-      game.destroy
-      
-      flash[:info] = "The Game you left now no longer exists. You can create a new Game or join a Game that is Open."
-        
-      render json: {
-        redraw_game_url: root_path
-      }
+      close_game
     elsif black_player_exists?
       if black_player_leaves_without_moving?
-        game.update_attribute(:black_player_id, nil)
+        update_game_for_white_after_black_leaves if current_player.id == game.white_player_id
         
-        if current_player.id == game.white_player_id
-          flash[:info] = "#{show_player(game.black_player_id)} has left your Game. Please wait for a new opponent."
-        
-          render json: {
-            redraw_game_url: game_path(game)
-          }
-        end
-        
-        if current_player.id == game.black_player_id
-          flash[:info] = "You left your Game with #{show_player(game.white_player_id)}. You can create a new Game or join a Game that is Open."
-        
-          render json: {
-            redraw_game_url: root_path
-          }
-        end
+        leave_game_open if current_player.id == game.black_player_id
       elsif both_players_have_moved?
         black_player = game_params[:black_player_id]
         white_player = game_params[:white_player_id]
@@ -49,17 +29,9 @@ class GamesController < ApplicationController
           game.update_attribute(:winning_player_id, game.white_player_id)
           
           if current_player.id == game.white_player_id
-            flash[:info] = "#{show_player(game.black_player_id)} has left your Game. Thus, you are the Winner by default."
-              
-            render json: {
-              redraw_game_url: game_path(game)
-            }
+            set_game_winner_for_remaining_player(game.black_player_id)
           else
-            flash[:info] = "You left your Game with #{show_player(game.white_player_id)}, who has been recorded as the Winner."
-            
-            render json: {
-              redraw_game_url: root_path
-            }
+            set_game_winner_for_leaving_player(game.white_player_id)
           end
         end
         
@@ -67,32 +39,17 @@ class GamesController < ApplicationController
           game.update_attribute(:winning_player_id, game.black_player_id)
           
           if current_player.id == game.black_player_id
-            flash[:info] = "#{show_player(game.white_player_id)} has left your Game. Thus, you are the Winner by default."
-              
-            render json: {
-              redraw_game_url: game_path(game)
-            }
+            set_game_winner_for_remaining_player(game.white_player_id)
           else
-            flash[:info] = "You left your Game with #{show_player(game.black_player_id)}, who has been recorded as the Winner."
-            
-            render json: {
-              redraw_game_url: root_path
-            }
+            set_game_winner_for_leaving_player(game.black_player_id)
           end
         end
       else
-        game.destroy
-      
-        flash[:info] = "Your Game has been cancelled. You can create a new Game or join a Game that is open."
-          
-        render json: {
-          redraw_game_url: root_path
-        }
+        cancel_game
       end
     elsif players_are_unique?
-    
       game.update_attributes game_params
-      redirect_to game_path(game)
+      redirect_to game
     end
   end
 
@@ -110,12 +67,12 @@ class GamesController < ApplicationController
       ( game.valid? && ( game.white_player_id != game_params[:black_player_id] ) ) ? true : false
     end
     
-    def white_player_leaving?
-      game_params[:white_player_id].eql?("") ? true : false
-    end
-    
     def black_player_exists?
       game.black_player_id ? true : false
+    end
+    
+    def white_player_leaving?
+      game_params[:white_player_id].eql?("") ? true : false
     end
     
     def black_player_leaving?
@@ -135,17 +92,65 @@ class GamesController < ApplicationController
     end
     
     def black_player_leaves_without_moving?
-      if black_player_leaving? && white_has_moved?
-        unless black_has_moved
-          return true
-        else
-          return false
-        end
-      end
+      black_has_moved? ? false : true if black_player_leaving?
     end
     
     def both_players_have_moved?
       ( white_has_moved? && black_has_moved? ) ? true : false
+    end
+    
+    def close_game
+      game.destroy
+      
+      flash[:info] = "The Game you left now no longer exists. You can create a new Game or join a Game that is Open."
+        
+      render json: {
+        redraw_game_url: root_path
+      }
+    end
+    
+    def cancel_game
+      game.destroy
+    
+      flash[:info] = "Your Game has been cancelled. You can create a new Game or join a Game that is open."
+        
+      render json: {
+        redraw_game_url: root_path
+      }
+    end
+    
+    def update_game_for_white_after_black_leaves
+      flash[:info] = "#{show_player(game.black_player_id)} has left your Game. Please wait for a new opponent."
+      game.update_attribute(:black_player_id, nil)
+    
+      render json: {
+        redraw_game_url: game_path(game)
+      }
+    end
+    
+    def leave_game_open
+      flash[:info] = "You left your Game with #{show_player(game.white_player_id)}. You can create a new Game or join a Game that is Open."
+      game.update_attribute(:black_player_id, nil)
+    
+      render json: {
+        redraw_game_url: root_path
+      }
+    end
+    
+    def set_game_winner_for_remaining_player(player)
+      flash[:info] = "#{show_player(player)} has left your Game. Thus, you are the Winner by default."
+        
+      render json: {
+        redraw_game_url: game_path(game)
+      }
+    end
+    
+    def set_game_winner_for_leaving_player(player)
+      flash[:info] = "You left your Game with #{show_player(player)}, who has been recorded as the Winner."
+      
+      render json: {
+        redraw_game_url: root_path
+      }
     end
 
 end
---
+++ test/integration/player_leave_test.rb
@@ -0,0 +1,50 @@
+require 'test_helper'
+
+class PlayerLeaveTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @player_1 = players(:player_1)
+    @player_2 = players(:player_2)
+    
+    log_in_as @player_1, { password: '123greetings' }
+    
+    post_via_redirect games_path, game: { white_player_id: @player_1.id }
+    
+    @game = assigns(:game)
+    
+    @white_pawn = @game.pieces.where(type: 'Pawn', color: 'white', x_coordinate: 7, y_coordinate: 8).last
+  end
+
+  test "should redirect white player to home page when game's only player" do
+    patch game_path(@game), game: { white_player_id: "" }
+    
+    assert_not Game.exists?(@game), "Game not deleted?"
+    
+    res = JSON.parse(response.body)
+    
+    assert_equal "/", res['redraw_game_url'], "White player not redirected to home page?"
+    
+    assert_not flash.empty?
+    assert_equal "The Game you left now no longer exists. You can create a new Game or join a Game that is Open.", flash[:info], "Message incorrect for white player only leaving?"
+  end
+
+  test "should redirect white player to home page after moving once" do
+    patch game_path(@game), game: { black_player_id: @player_2.id }
+    assert_redirected_to @game
+    @game.reload
+    
+    put game_piece_path(game_id: @game, id: @white_pawn, piece: { id: @white_pawn, x_coordinate: 6, y_coordinate: 8 })
+    
+    patch game_path(@game), game: { white_player_id: "" }
+    
+    assert_not Game.exists?(@game), "Game not deleted?"
+    
+    res = JSON.parse(response.body)
+    
+    assert_equal "/", res['redraw_game_url'], "White player not redirected to home page?"
+    
+    assert_not flash.empty?
+    assert_equal "Your Game has been cancelled. You can create a new Game or join a Game that is open.", flash[:info], "Message incorrect for white player leaving (after 1 move)?"
+  end
+
+end
