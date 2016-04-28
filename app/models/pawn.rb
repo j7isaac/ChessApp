a931fd526b11f4e3ae7a62f9cf5269eaaccef51f
j7isaac <j7issac@gmail.com>
2016-04-28 17:59:16 -0500

    Switched all references to 'x' and 'y' variables and adjusted tests
    - These changes do include some Rubocop cleanup.
    - Not all Rubocop cleanup was made. Some changes are needed to the
    length of certain methods.

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,41 +1,38 @@
 class Pawn < Piece
-
   def valid_move?(x, y)
-  # piece isn't required to move (turn not forfeited)
+    # piece isn't required to move (turn not forfeited)
     return true if x == x_coordinate && y == y_coordinate
-  
-  # rule: pawn can't move backwards
-  # white: X7 - X6 (a backward move) is > 0, so return false
-    return false if color.eql?('white') && ( x - x_coordinate ) > 0
-  # black: X3 - X2 (a backward move) is > 0, so return false
-    return false if color.eql?('black') && ( x_coordinate - x ) > 0
+    # rule: pawn can't move backwards
+    # black: y7 - y6 (a backward move) is > 0, so return false
+    return false if color.eql?('black') && (y - y_coordinate) > 0
+    # white: y3 - y2 (a backward move) is > 0, so return false
+    return false if color.eql?('white') && (y_coordinate - y) > 0
 
-  # rule: pawn can move forward two spaces from its starting point
-  # white: X7 > X5 (5 == ( 7 - 2 ))
-  # => white pawn hasn't moved if its x_coordinate is 7 (since it can't move backwards)
-  # black: X2 > X4 (4 == ( 2 + 2 ))
-  # => black pawn hasn't moved if its x_coordinate is 2 (since it can't move backwards)
-    if x == ( x_coordinate - 2 ) && ( color.eql?('white') && x_coordinate == 7 ) ||
-       x == ( x_coordinate + 2 ) && ( color.eql?('black') && x_coordinate == 2 )
-      return true if ( ( x - x_coordinate ).abs == 2 ) && ( y_coordinate == y )
+    # rule: pawn can move forward two spaces from its starting point
+    # black: y7 > y5 (5 == ( 7 - 2 ))
+    # => black pawn hasn't moved if its x_coordinate is 7 (since it can't move backwards)
+    # white: y2 > y4 (4 == ( 2 + 2 ))
+    # => white pawn hasn't moved if its x_coordinate is 2 (since it can't move backwards)
+    if y == (y_coordinate - 2) && (color.eql?('black') && y_coordinate == 7) ||
+       y == (y_coordinate + 2) && (color.eql?('white') && y_coordinate == 2)
+      return true if ((y - y_coordinate).abs == 2) && (x_coordinate == x)
     end
 
-  # rule: pawn captures enemy pieces with diagonal move
-  # Collect remaining enemy pieces
+    # rule: pawn captures enemy pieces with diagonal move
+    # Collect remaining enemy pieces
     enemy_pieces = game.pieces.where(captured?: false).where.not(color: color)
 
-  # Check if an enemy piece occupies the targeted destination  
+    # Check if an enemy piece occupies the targeted destination
     enemy_pieces.each do |enemy_piece|
-      if ( ( x - x_coordinate ).abs == 1 && ( y - y_coordinate ).abs == 1 ) && 
-         ( enemy_piece.x_coordinate == x && enemy_piece.y_coordinate == y )
+      if ((x - x_coordinate).abs == 1 && (y - y_coordinate).abs == 1) &&
+         (enemy_piece.x_coordinate == x && enemy_piece.y_coordinate == y)
         return true
       end
     end
 
-  # rule: pawn moves forward one space
-    if y_coordinate == y
-      return true if ( color.eql?('white') || color.eql?('black') ) && ( x - x_coordinate ).abs == 1
+    # rule: pawn moves forward one space
+    if x_coordinate == x
+      return true if (color.eql?('black') || color.eql?('white')) && (y - y_coordinate).abs == 1
     end
   end
-  
 end
--- test/models/pawn_test.rb
+++ test/models/pawn_test.rb
@@ -1,69 +1,67 @@
 require 'test_helper'
 
 class PawnTest < ActiveSupport::TestCase
-  
   def setup
     game = games(:one)
-    
+
     @white_pawns = {}
     @black_pawns = {}
 
     (1..8).each do |i|
-      @white_pawns[i] = Pawn.create(game: game, x_coordinate: 7, y_coordinate: i, color: 'white')
-      @black_pawns[i] = Pawn.create(game: game, x_coordinate: 2, y_coordinate: i, color: 'black')
+      @white_pawns[i] = Pawn.create(game: game, x_coordinate: i, y_coordinate: 2, color: 'white')
+      @black_pawns[i] = Pawn.create(game: game, x_coordinate: i, y_coordinate: 7, color: 'black')
     end
   end
-  
-# tests for white pawns
-  
-  test "each white pawn may stay where it is" do
+
+  # tests for white pawns
+
+  test 'each white pawn may stay where it is' do
     @white_pawns.each do |k, white_pawn|
-      assert white_pawn.valid_move?(7, k), "p#{k}w can't stay in X7/Y#{k}?"
+      assert white_pawn.valid_move?(k, 2), "p#{k}w can't stay in X#{k}/Y2?"
     end
   end
 
-  test "each white pawn should move one square from its starting position" do
+  test 'each white pawn should move one square from its starting position' do
     @white_pawns.each do |k, white_pawn|
-      assert white_pawn.valid_move?(6, k), "p#{k}w can't move from X7/Y#{k} to X6/Y#{k}?"
+      assert white_pawn.valid_move?(k, 3), "p#{k}w can't move from X#{k}/Y2 to X#{k}/Y3?"
     end
   end
-  
-  test "each white pawn should move two squares from its starting position" do
+
+  test 'each white pawn should move two squares from its starting position' do
     @white_pawns.each do |k, white_pawn|
-      assert white_pawn.valid_move?(5, k), "p#{k}w can't move from X7/Y#{k} to X5/Y#{k}?"
+      assert white_pawn.valid_move?(k, 4), "p#{k}w can't move from X#{k}/Y2 to X#{k}/Y4?"
     end
   end
 
-  test "each white pawn can not move backwards" do
+  test 'each white pawn can not move backwards' do
     @white_pawns.each do |k, white_pawn|
-      refute white_pawn.valid_move?(8, k), "p#{k}w can move from X7/Y#{k} to X8/Y#{k}?"
+      refute white_pawn.valid_move?(k, 1), "p#{k}w can move from X#{k}/Y2 to X#{k}/Y1?"
     end
   end
 
-# tests for black pawns
+  # tests for black pawns
 
-  test "each black pawn may stay where it is" do
+  test 'each black pawn may stay where it is' do
     @black_pawns.each do |k, black_pawn|
-      assert black_pawn.valid_move?(2, k), "p#{k}b can't stay in X2/Y#{k}?"
-    end
-  end
-  
-  test "each black pawn should move one square from its starting position" do
-    @black_pawns.each do |k, black_pawn|
-      assert black_pawn.valid_move?(3, k), "p#{k}b can't move from X2/Y#{k} to X3/Y#{k}?"
-    end
-  end
-  
-  test "each black pawn should move two squares from its starting position" do
-    @black_pawns.each do |k, black_pawn|
-      assert black_pawn.valid_move?(4, k), "p#{k}b can't move from X2/Y#{k} to X4/Y#{k}?"
+      assert black_pawn.valid_move?(k, 7), "p#{k}b can't stay in X#{k}/Y7?"
     end
   end
 
-  test "each black pawn can not move backwards" do
+  test 'each black pawn should move one square from its starting position' do
     @black_pawns.each do |k, black_pawn|
-      refute black_pawn.valid_move?(1, k), "p#{k}b can move from X2/Y#{k} to X1/Y#{k}?"
+      assert black_pawn.valid_move?(k, 6), "p#{k}b can't move from X#{k}/Y7 to X#{k}/Y6?"
     end
   end
 
+  test 'each black pawn should move two squares from its starting position' do
+    @black_pawns.each do |k, black_pawn|
+      assert black_pawn.valid_move?(k, 5), "p#{k}b can't move from X#{k}/Y7 to X#{k}/Y5?"
+    end
+  end
+
+  test 'each black pawn can not move backwards' do
+    @black_pawns.each do |k, black_pawn|
+      refute black_pawn.valid_move?(k, 8), "p#{k}b can move from X#{k}/Y7 to X#{k}/Y8?"
+    end
+  end
 end
