e904a54828fe56f97a8900dc55b679605a65edb9
j7isaac <j7issac@gmail.com>
2016-05-30 16:12:12 -0500

    Fixed Teammates section and background color

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  # Data for teammates section on landing page.
+  def teammates
+    [
+      ['Travis Johnson', 'Senior Developer', 'assets/face.jpg'],
+      ['Justin Joseph', 'Junior Developer', 'assets/face.jpg'],
+      ['Jaime Moreno', 'Junior Developer', 'assets/face.jpg'],
+      ['Sandra Harrasser', 'Junior Developer', 'assets/face.jpg'],
+      ['Sebastian Bedout', 'Junior Developer', 'assets/face.jpg'],
+      ['Martin Loekito', 'Junior Developer', 'assets/face.jpg']
+    ]
+  end
 end
